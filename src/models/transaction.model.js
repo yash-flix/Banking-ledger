1da410a2b21511239d6ff4ef494e6e308d852585
Yash Rane <yash.tushar13@gmail.com>
2026-02-19 00:16:06 -0800

    implement ledger schema

---
+++ src/models/transaction.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+    fromAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "account" ,
+        required : [true , "Transaction must be associated with a fromAccount"],
+        index : true
+    },
+    toAccount : 
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "account" ,
+        required : [true , "Transaction must be associated with a toAccount"],
+        index : true      
+    },
+    status : 
+    {
+        type : String ,
+        enum: {
+            values : ["PENDING " , "COMPLETED" , "FAILED" ,"REVERSED"],
+            message:"Status can be either PENDING , COMPLETED , FAILED or REVERESED"
+        },
+        default : true
+    },
+    amount : 
+    {
+        type:Number, 
+        required : [true , "Amount is required for creating a transaction"],
+        min : [0 , "Transaction amount cannot be negative"] 
+    },
+    idempotencyKey:
+    {
+        type:String , 
+        required: [true , "Idempotency key is required for creating a transaction"],
+        index:true,
+        unique: true
+    }
+} ,{
+    timestamps:true
+})
+
+
+const transactionModel = mongoose.model("transaction" , transactionSchema);
+
+module.exports = transactionModel;
